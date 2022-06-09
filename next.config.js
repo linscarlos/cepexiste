cc8aa89be926828482e327e96a62262cb03d71b6
Carlos Lins <carloslinsdev@gmail.com>
2022-06-09 14:16:23 -0300

    Deploy try

--- next.config.js
+++ next.config.js
@@ -4,11 +4,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
